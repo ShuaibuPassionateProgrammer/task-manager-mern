7802ec62cb7cab241f4d2e9a164f770d4e6e9cbb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-27 21:03:45 +0100

    Setting up the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Server started...");
+});
+
 app.listen(3001, () => console.log(`Server is up running on port:3001`));

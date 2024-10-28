e3306020b4aefe0297b801e41225e91d5a6b07e7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 10:44:26 +0100

    Working on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Task from "./models/task.model.js";
+import TaskRoutes from "./routes/task.route.js";
 
 dotenv.config();
 const port = process.env.PORT || 4000;

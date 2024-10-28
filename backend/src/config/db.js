5ab0d613cea1405b17f37bb904514b5423fe939f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 09:13:35 +0100

    Setting up the database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async (req, res) => {
     try {
+        const conn = mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     }
     catch(error) {
         console.error("Error in database connection: " + error?.message);

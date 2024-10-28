205bdfd084db828c737b46e530cc40ef32c5183b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 09:12:30 +0100

    Setting up the database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async (req, res) => {};
+const connectDB = async (req, res) => {
+    try {
+    }
+    catch(error) {
+        console.error("Error in database connection: " + error?.message);
+        process.exit(1);
+    }
+};
 
 export { connectDB };

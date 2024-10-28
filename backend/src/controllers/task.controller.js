bf6ee1f6524b3a637bfa938b3d7bfabb048981f7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 10:29:21 +0100

    Setting up the task controller

---
+++ backend/src/controllers/task.controller.js
@@ -0,0 +1,16 @@
+import Task from "../models/task.model.js";
+
+const createTask = async () => {};
+
+const getTask = async () => {};
+
+const updateTask = async () => {};
+
+const deleteTask = async () => {};
+
+export {
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask 
+};

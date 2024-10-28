4dd9403df9cba8b8d26898cc2825b931dcf54592
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 10:59:54 +0100

    Working on task controller

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -21,6 +21,7 @@ const createTask = async (req, res) => {
 const allTask = async (req, res) => {
     try {
         const tasks = await Task.find();
+        res.status(200).json(tasks);
     }
     catch (error) {
         res.status(500).json({ error: error?.message });

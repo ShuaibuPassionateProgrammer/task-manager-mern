e3db687be349610672ff4939ebc621e8dc6c523b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 09:07:57 +0100

    Setting up the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Server started...");
 });

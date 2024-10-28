12d7d98c03870b80f31fbdc7d30a95acd5812a48
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-28 10:32:01 +0100

    Setting up the task router

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { createTask, getTask, updateTask, deleteTask } from "../controllers/task.controller.js";
 
 const router = express.Router();
+
+
 
 export default router;

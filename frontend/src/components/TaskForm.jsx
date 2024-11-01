2098fb1032faaef9d3003a602f289e9cd8ffa413
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-01 07:06:01 +0100

    Working on frontend

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,7 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { createTask } from "../api.js";
 
 const TaskForm = () => {
-    return <h1>Task Form</h1>
+    const [task, setTasks] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="mb-6">
+            <div className="mb-4">
+                <input 
+                    type="text"
+                    placholder="Task Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+            </div>
+            <div className="mb-4">
+                <textarea 
+                    type="text"
+                    placeholder="Task Description"
+                    onChange={e => setDescription(e.target.value)}
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    rows="3"
+                ></textarea>
+            </div>
+            <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">
+                Add Task
+            </button>
+        </form>
+    );
 };
 
 export default TaskForm;

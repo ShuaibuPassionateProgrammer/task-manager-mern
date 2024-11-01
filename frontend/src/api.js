a9294627eb6cd652e38df0ad5ada6529eacc73e4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-01 06:50:44 +0100

    Working on frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,32 +1,31 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/tasks";
+const API_URL = "http://localhost:5000/api/tasks";
 
-// Get all task
+// Get all tasks
 export const fetchTasks = async () => {
     const response = await axios.get(API_URL);
     return response.data;
 };
 
 // Create a new task
-export const createTask = async () => {
-    const response = await axios.post(API_URL);
+export const createTask = async (task) => {
+    const response = await axios.post(API_URL, task);
     return response.data;
 };
 
-// Update a Task
+// Update a task
 export const updateTask = async (id, updatedTask) => {
     const response = await axios.put(`${API_URL}/${id}`, updatedTask);
     return response.data;
 };
 
-// Delete a Task
+// Delete a task
 export const deleteTask = async (id) => {
     try {
-        await axios.delete(`${API_URL}/${$id}`);
-    }
-    catch (error) {
-        console.error("Error deleting task: " + error?.message);
-        throw error;
+      await axios.delete(`${API_URL}/${id}`);
+    } catch (error) {
+      console.error("Error deleting task:", error);
+      throw error;
     }
 };

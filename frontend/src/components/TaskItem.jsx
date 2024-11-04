2b2bbe2abdec8e75d797837874fd868a8266f68a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-04 08:19:08 +0100

    Working on frontend

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { deleteTask, updateTask } from "../api";
 
-const TaskItem = () => {
+const TaskItem = ({ task }) => {
     const [isEditing, setIsEditing] = useState(false);
+    const [editTitle, setEditTitle] = useState(task.title);
 
     return (
         <div className="bg-white shadow-md rounded-lg p-5 hover:shadow-lg transition-shadow duration-300">
